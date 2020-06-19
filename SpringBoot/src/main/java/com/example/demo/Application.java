b1213d49fbbc509b1bf1a4b6ea26631e77015a7b
vijaysekhar06 <palaganiayyappa@gmail.com>
2020-06-19 18:15:55 +0530

    first commit

---
+++ SpringBoot/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("kkkkkkkkkkkkkkkkkkkkkk");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
